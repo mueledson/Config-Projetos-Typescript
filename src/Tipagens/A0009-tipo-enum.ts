dbe35dd8e77ad5ea2f28c42ed5aa3205c818ac1a
edson@app100.com.br <mueledson>
2023-06-18 12:48:16 -0300

    Mais Tipagens

--- src/Tipagens/A0009-tipo-enum.ts
+++ src/Tipagens/A0009-tipo-enum.ts
@@ -1,5 +1,21 @@
 enum Cores {
-    AZUL,
-    VERMELHO,
-    AMARELO
+    AZUL = 1,    //indice: 0 com esse = 1 ele muda de 0 para 1
+    VERMELHO,    //indice: 1                                 2
+    AMARELO,     //indice: 2                                 3
+    ROXO,        //indice: 3                                 4
 }
+
+enum Cores {
+    BRANCO = 5,
+    PRETO,
+    CINZA
+}
+
+// Frase elaborada console.log(`Indice ${Cores.AZUL} é ${Cores[1]}`)
+// console.log(Cores)
+
+function escolhaACor(cor: Cores): void {
+    console.log(Cores[cor])
+}
+
+escolhaACor(Cores.PRETO)
---
+++ src/Tipagens/A0010-tipo-unknow.ts
@@ -0,0 +1,19 @@
+//O unknown ele é tipo um any só que ele é mais seguro
+
+//NULL
+let x;
+x = 100
+x = 'react'
+x = '50'
+const y = 100;
+
+console.log(x + y)
+
+//UNKNOWN
+let x2;
+x2 = 100
+x2 = 'react'
+x2 = 50
+const y2 = 100;
+
+console.log(x2 + y2)
